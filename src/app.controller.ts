eb507a3ce8e3e45a493bc80fd5f2dc17c0a31cd4
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-12-08 09:38:02 -0300

    aula(46): message acknowledgements

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,39 @@
 import { Controller, Logger } from '@nestjs/common';
-import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
+import {
+  Ctx,
+  EventPattern,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { Categoria } from './interfaces/categorias/categoria.interface';
 
 @Controller()
 export class AppController {
   private readonly logger = new Logger(AppController.name);
+  private ackErrors = [];
   constructor(private readonly appService: AppService) {}
 
   @EventPattern('criar-categoria')
-  async criarCategoria(@Payload() categoria: Categoria) {
+  async criarCategoria(
+    @Payload() categoria: Categoria,
+    @Ctx() context: RmqContext,
+  ) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
     this.logger.log(`categoria: ${JSON.stringify(categoria)}`);
-    await this.appService.criarCategoria(categoria);
+    try {
+      await this.appService.criarCategoria(categoria);
+      await channel.ack(originalMessage);
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+      this.ackErrors.forEach(async (ackError) => {
+        if (error.message.includes(ackError)) {
+          await channel.ack(originalMessage);
+        }
+      });
+    }
   }
 
   @MessagePattern('consultar-categorias')
--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 
 dotenv.config();
 const logger = new Logger('main');
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
-    transport: +process.env.TRANSPORT,
+    transport: Transport.RMQ,
     options: {
-      urls: [process.env.SERVER_URL],
-      queue: process.env.QUEUE_NAME,
+      urls: [`amqp://<user>:<pass>@<host>:5672/<internal_host>`],
+      noAck: false,
+      queue: '<queue_name>',
     },
   });
   logger.log('microservice is listening');
   await app.listen();
 }
+
 bootstrap();
